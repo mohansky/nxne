a75625c3416d4792319c059e506db54f9cdb98e9
mohansky <38560439+mohansky@users.noreply.github.com>
2022-06-21 21:16:29 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    images: {
+      formats: ['image/avif', 'image/webp'],
+    },
+  }
+
+// module.exports = (phase, defaultConfig) => {
+//     return withBundleAnalyzer(defaultConfig)
+//   }
+
+// const withCss = require("@zeit/next-css");
+// const withPurgeCss = require("next-purgecss");
+  
+// module.exports = withCss(withPurgeCss());
