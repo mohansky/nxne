95a68d1b085cbc7de7501ea0dc97da364055cd30
mohansky <38560439+mohansky@users.noreply.github.com>
2022-06-22 14:08:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 module.exports = {
+    reactStrictMode: true,
     images: {
       formats: ['image/avif', 'image/webp'],
     },
   }
-
-// module.exports = (phase, defaultConfig) => {
-//     return withBundleAnalyzer(defaultConfig)
-//   }
-
-// const withCss = require("@zeit/next-css");
-// const withPurgeCss = require("next-purgecss");
-  
-// module.exports = withCss(withPurgeCss());
