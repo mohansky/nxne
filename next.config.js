c025ba77b65d674df9399d2cece46a6e01d33bf1
mohansky <38560439+mohansky@users.noreply.github.com>
2022-06-22 14:38:46 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
+  reactStrictMode: true,
+  images: {
       formats: ['image/avif', 'image/webp'],
-    },
+    }
+  },
+  {
+    "plugins": [
+      "postcss-flexbugs-fixes",
+      [
+        "postcss-preset-env",
+        {
+          "autoprefixer": {
+            "flexbox": "no-2009"
+          },
+          "stage": 3,
+          "features": {
+            "custom-properties": false
+          }
+        }
+      ],
+      [
+        '@fullhuman/postcss-purgecss',
+        {
+          content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}'
+          ],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          safelist: ["html", "body"]
+        }
+      ],
+    ]
   }
