38adadbc6e29df4f3fcb4d9d362be28c67f65826
mohansky <38560439+mohansky@users.noreply.github.com>
2022-06-25 19:24:08 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,35 +3,35 @@ module.exports = {
   images: {
       formats: ['image/avif', 'image/webp'],
     }
-  },
-  {
-    "plugins": [
-      "postcss-flexbugs-fixes",
-      [
-        "postcss-preset-env",
-        {
-          "autoprefixer": {
-            "flexbox": "no-2009"
-          },
-          "stage": 3,
-          "features": {
-            "custom-properties": false
-          }
-        }
-      ],
-      [
-        '@fullhuman/postcss-purgecss',
-        {
-          content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}'
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          safelist: ["html", "body"]
-        }
-      ],
-    ]
   }
+  // {
+  //   "plugins": [
+  //     "postcss-flexbugs-fixes",
+  //     [
+  //       "postcss-preset-env",
+  //       {
+  //         "autoprefixer": {
+  //           "flexbox": "no-2009"
+  //         },
+  //         "stage": 3,
+  //         "features": {
+  //           "custom-properties": false
+  //         }
+  //       }
+  //     ],
+  //     [
+  //       '@fullhuman/postcss-purgecss',
+  //       {
+  //         content: [
+  //             './pages/**/*.{js,jsx,ts,tsx}',
+  //             './components/**/*.{js,jsx,ts,tsx}'
+  //         ],
+  //         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  //         safelist: ["html", "body"]
+  //       }
+  //     ],
+  //   ]
+  // }
 
   const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
