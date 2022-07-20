e048464ebb979ca5e41d36d9a06dc516176bc7c4
mohansky <38560439+mohansky@users.noreply.github.com>
2022-07-20 18:51:52 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -4,34 +4,6 @@ module.exports = {
       formats: ['image/avif', 'image/webp'],
     }
   }
-  // {
-  //   "plugins": [
-  //     "postcss-flexbugs-fixes",
-  //     [
-  //       "postcss-preset-env",
-  //       {
-  //         "autoprefixer": {
-  //           "flexbox": "no-2009"
-  //         },
-  //         "stage": 3,
-  //         "features": {
-  //           "custom-properties": false
-  //         }
-  //       }
-  //     ],
-  //     [
-  //       '@fullhuman/postcss-purgecss',
-  //       {
-  //         content: [
-  //             './pages/**/*.{js,jsx,ts,tsx}',
-  //             './components/**/*.{js,jsx,ts,tsx}'
-  //         ],
-  //         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  //         safelist: ["html", "body"]
-  //       }
-  //     ],
-  //   ]
-  // }
 
   const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
