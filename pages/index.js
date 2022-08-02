66c9cd1319ab668cadda8700e839db2e39d5cea3
mohansky <38560439+mohansky@users.noreply.github.com>
2022-08-02 17:05:32 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-// import { getAllPosts } from "../lib/api";
+
 import Head from "next/head";
 import { CMS_NAME, imgblurDataURL, EXAMPLE_PATH,  HOME_OG_IMAGE_URL } from "../lib/constants";
 import About from "../components/about";
 import MissionAccordion from "../components/mission-accordion";
 import CTA from "../components/cta";
 import Testimonials from "../components/testimonials";
-import TourTypes from "../components/tour-types";
+import TourTypes from "../components/tour-types"; 
+import Region from "../components/region";
 
 export default function Index( ) {
    
@@ -16,9 +17,10 @@ export default function Index( ) {
       </Head> 
       <TourTypes />
       <About />
-      <MissionAccordion />
+      {/* <MissionAccordion /> */}
+      <Region/>
       <CTA />
-      <Testimonials />
+      <Testimonials /> 
     </>
   );
 } 
